5a750f24bfb793a432a71fbc0973d854bb21d6d4
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-10 00:24:23 +0530

    [SaiVijayakumar]Display Data In Json server

---
+++ TestingForAddressBook/AddressBookData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingForAddressBook
+{
+    public  class AddressBookData
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public Int64 zip { get; set; }
+        public Int64 phoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ TestingForAddressBook/AddressBookTesting.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace TestingForAddressBook
+{
+    [TestClass]
+    public class AddressBookTesting
+    {
+        //seting the restclient as null
+        RestClient client = null;
+        [TestInitialize]
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:3000");
+        }
+        public IRestResponse GetAllEmployees()
+        {
+            //Define method Type
+            RestRequest request = new RestRequest("/AddressBookContacts", Method.GET);
+            //Eexcute request
+            IRestResponse response = client.Execute(request);
+            //Return the response
+            return response;
+        }
+        //UC 1: Getting all the employee details from json server
+        [TestMethod]
+        public void OnCallingGetMethodWeAreReturningContactData()
+        {
+            IRestResponse response = GetAllEmployees();
+            //Deserialize json object to List
+            var jsonObject = JsonConvert.DeserializeObject<List<AddressBookData>>(response.Content);
+            foreach(var i in jsonObject)
+            {
+                Console.WriteLine($"Id : {i.id}| Name : {i.firstName+" "+i.lastName}| Address : {i.address+" "+i.city+" "+i.state} | Zip : {i.zip}|" +
+                    $"PhoneNumber:{i.phoneNumber}|Email:{i.Email}");
+            }
+            //Check by count 
+            Assert.AreEqual(3, jsonObject.Count);
+            //Check by status code 
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
